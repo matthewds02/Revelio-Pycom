5ff19b9a0511b3e61cdbfeb76a865c0ff6c00e7b
Matthew <descheppermatthew@gmail.com>
2021-03-05 11:08:49 +0100

    test

--- lib/Wifi.py
+++ lib/Wifi.py
@@ -1,11 +1,13 @@
 #https://docs.pycom.io/tutorials/networks/wlan/
 from network import WLAN
 import machine
-wlan = WLAN(mode=WLAN.STA)
 
-wlan.connect(ssid='IoT', auth=(WLAN.WPA2, 'KdGIoT92!'))
-while not wlan.isconnected():
-    machine.idle()
-print("WiFi connected succesfully")
-print(wlan.ifconfig())
+def Wifi():
+    wlan = WLAN(mode=WLAN.STA)
+
+    wlan.connect(ssid='IoT', auth=(WLAN.WPA2, 'KdGIoT92!'))
+    while not wlan.isconnected():
+        machine.idle()
+    print("WiFi connected succesfully")
+    print(wlan.ifconfig())
 
