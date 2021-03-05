e1960231cc16b4e93c14fe501fe532fd12e4cc10
Matthew <descheppermatthew@gmail.com>
2021-03-05 10:53:42 +0100

    test

---
+++ lib/ultrasonic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Info about the project
+"""
+
+# Generic/Built-in
+import
+
+...
+
+# Owned
+__author__ = "Matthew De Schepper"
+__email__ = "matthew.deschepper@student.kdg.be"
+__status__ = "Dev"
+
+
+def main():
+
+
+if __name__ == '__main__':  # code to execute if called from command-line
+    main()
