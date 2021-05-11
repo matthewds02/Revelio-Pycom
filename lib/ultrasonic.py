b8c46e2f14dcba8de52f2278acceea9d8b486bd1
Matthew <descheppermatthew@gmail.com>
2021-05-11 21:00:05 +0200

    optimalisatie

--- lib/ultrasonic.py
+++ lib/ultrasonic.py
@@ -1,10 +1,7 @@
 from machine import UART
 
-
-
 uart = UART(1)
 uart.init(baudrate=9600, bits=8, parity=None, stop=1, timeout_chars=190)
-
 
 def Sensor():
     while True:
@@ -13,5 +10,7 @@ def Sensor():
         DATA_H = int(uart.read(2)[0])
         DATA_L = int(uart.read(3)[0])
         SUM = DATA_H*256 + DATA_L
-        if int(SUM/10) < 1000:
+        if int(SUM/10) < 1000 and int(SUM/10) > 10:
             print("LENGTE",int(SUM/10), "cm")
+        if int(SUM/10) <= 10:
+            print("Je komt wel heel dichtbij..., namelijk ",int(SUM/10), "cm")
